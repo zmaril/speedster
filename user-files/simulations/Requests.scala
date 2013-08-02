a5b46e6283f0766a0cfcaf9b601cde721e8cbba0
Zack Maril <zack@zacharymaril.com>
2013-08-02 21:31:22 +0000

    Scripts using the ackermann function.

--- user-files/simulations/Requests.scala
+++ user-files/simulations/Requests.scala
@@ -28,9 +28,9 @@ object Requests{
 	val httpProtocol = http.baseURL(URL+":"+PORT)
 
 
-	val num_users   = 1000
+	val num_users   = 10
   val num_seconds = 1
-  val num_repeats = 100
+  val num_repeats = 1000
 
 	def script_request(script:String,result:String):
 			io.gatling.core.structure.ChainBuilder = {
@@ -42,7 +42,7 @@ object Requests{
 					 .check(status.is(200),regex(result)))
 		}
 		else{
-			exec(http("Script Request")
+				exec(http("Script Request")
            .post("/execute")
            .header("Accept", "application/json")
            .header("Content-Type", "application/json")
@@ -51,10 +51,14 @@ object Requests{
 		}
 	}
 
-	var trivial_request = script_request("123456789","123456789")
+	var ack = "def ack (m,n){m == 0 ? n + 1 : n == 0 ? ack(m-1, 1) : ack(m-1, ack(m, n-1));}\n"
 
-	var short_request    = script_request("sleep(1);123456789","123456789")
-	var medium_request   = script_request("sleep(10);123456789","123456789")
-	var long_request     = script_request("sleep(100);123456789","123456789")
-	var forever_request  = script_request("sleep(1000);123456789","123456789")
+	def acked_for(n:Integer): String = {ack+"for(int i =1; i<"+n+"; i++){ack(3,7)};\n 123456788+1"}
+	
+
+	var trivial_request  = script_request(ack+"123456788+1","123456789")
+	var short_request    = script_request(acked_for(1),"123456789")
+	var medium_request   = script_request(acked_for(10),"123456789")
+	var long_request     = script_request(acked_for(100),"123456789")
+	var forever_request  = script_request(acked_for(1000),"123456789")
 }
