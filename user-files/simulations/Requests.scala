8923ebd9326c3ea929133491210466f4185c28f6
Zack Maril <zack@zacharymaril.com>
2013-08-16 17:01:20 +0000

    Stream lining of tests for local versus split tests.

--- user-files/simulations/Requests.scala
+++ user-files/simulations/Requests.scala
@@ -12,68 +12,14 @@ object Requests{
 	
 	val APP = "rexster"//sys.env("APP")
 	val URL = "http://ec2-54-225-75-101.compute-1.amazonaws.com"
-	var PORT, URI ="";
-
-	if (APP == "rexster"){
-    PORT = "8182"
-		URI  = "/graphs/graph/tp/gremlin"
-	}
-	else if (APP == "speedster"){
-    PORT = "8080"
-		URI  = "/execute"
-	}
-	else{
-		throw new Exception("'APP' wasn't set in the environment.")
-	}
+  val PORT = "8182"
+	val URI  = "/graphs/graph/tp/gremlin"
 	val httpProtocol = http.baseURL(URL+":"+PORT)
 
 
-	val num_users   = 1
+	val num_users   = 100
   val num_seconds = 1
-  val num_repeats = 1
-
-	def script_request(script:String,result:String):
-			io.gatling.core.structure.ChainBuilder = {
-		if(APP=="rexster"){
-			exec(http("Script Request")
-					 .post(URI)
-					 .header("Accept", "application/json")
-					 .param("script",script)
-					 .check(status.is(200),regex(result)))
-		}
-		else{
-				exec(http("Script Request")
-           .post("/execute")
-           .header("Accept", "application/json")
-           .header("Content-Type", "application/json")
-           .body(StringBody("{\"script\":\""+script+"\"}\""))
-					 .check(status.is(200),regex(result)))
-		}
-	}
-
-	var ack = "def ack (m,n){m == 0 ? n + 1 : n == 0 ? ack(m-1, 1) : ack(m-1, ack(m, n-1));}\n"
-
-	def acked_for(n:Integer): String = {ack+"for(int i =1; i<"+n+"; i++){ack(3,7)};\n 123456788+1"}
-	
-
-	var trivial_request  = script_request(ack+"123456788+1","123456789")
-	var short_request    = script_request(acked_for(1),"123456789")
-	var medium_request   = script_request(acked_for(10),"123456789")
-	var long_request     = script_request(acked_for(100),"123456789")
-	var forever_request  = script_request(acked_for(1000),"123456789")
-
-	//Load up the graph
-	//Query the graph a bunch
-	//Remove said graph
-	var clean_request = exec(http("Clean Graph Request") 
-													.post(URI)
-													.header("Accept", "application/json")
-													.param("script","""
-																 if(g.V().count() == 0){
-																 GraphSONReader.inputGraph(g,new FileInputStream(new File("/home/ubuntu/marvel.graphson")));
-																 }
-																 123456788+1""")
-															 .check(status.is(200),regex("123456789")))
+  val num_repeats = 1000
 
 	var names = csv("/home/ubuntu/names.csv").random
 	var name_request = feed(names).exec(http("Named Request") 													
